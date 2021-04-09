73fb639b421d0cc88a958f9fd5dac14bcb022955
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2021-04-09 10:07:55 +0300

    Created sln file for Bullshit Studio

--- Roominator/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ Roominator/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,70 +13,70 @@ namespace Roominator.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Roominator\Roominator\_Imports.razor"
+#line 1 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Roominator\Roominator\_Imports.razor"
+#line 2 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Roominator\Roominator\_Imports.razor"
+#line 3 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Roominator\Roominator\_Imports.razor"
+#line 4 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Roominator\Roominator\_Imports.razor"
+#line 5 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Roominator\Roominator\_Imports.razor"
+#line 6 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Roominator\Roominator\_Imports.razor"
+#line 7 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Roominator\Roominator\_Imports.razor"
+#line 8 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Roominator\Roominator\_Imports.razor"
+#line 9 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Roominator;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Roominator\Roominator\_Imports.razor"
+#line 10 "C:\Users\ycatdev\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Roominator.Shared;
 
 #line default
