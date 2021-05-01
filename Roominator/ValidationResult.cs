9e76f2d4243a92c0a0df59c1c74f579fef2b1a18
Maksim-Balanyuk <79699031+Maksim-Balanyuk@users.noreply.github.com>
2021-05-01 23:16:48 +0300

    Login with database

--- Roominator/ValidationResult.cs
+++ Roominator/ValidationResult.cs
@@ -9,8 +9,8 @@ namespace Roominator
         string password;
         string password_copy;
         string email;
-        bool correctEmail;
-        bool correctPassword;
+        public bool correctEmail;
+        public bool correctPassword;
 
         public ValidationResult(string password, string password_copy, string email) {
             this.password = password;
@@ -21,18 +21,33 @@ namespace Roominator
         }
 
         public bool validate() {
-            return true;
+            if (password.Length >= 3 && password.Length <= 50)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool isEverythingCorrect() {
-            return correctEmail && correctPassword;
+            DataTable res = Program.databaseManager.ExecQuery($"SELECT * FROM public.user WHERE public.user.user_email = '{email}'");
+            if (res.Rows.Count == 1)
+            {
+                correctEmail = true;
+                res = Program.databaseManager.ExecQuery($"SELECT user_password FROM public.user WHERE public.user.user_email = '{email}'");
+                if (res.Rows[0][0].ToString() == password)
+                {
+                    correctPassword = true;
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool userExists() {
-            DataTable res  = Program.databaseManager.ExecQuery($"SELECT User.email FROM User WHERE User.email = '{email}'");
+            DataTable res  = Program.databaseManager.ExecQuery($"SELECT * FROM public.user WHERE public.user.user_email = '{email}'");
             if (res.Rows.Count == 0)
-                return true;
-            return false;
+                return false;
+            return true;
         }
     }
 }
--- Roominator/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ Roominator/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace Roominator.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 1 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 2 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 3 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 4 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 5 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 6 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 7 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 8 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 9 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Roominator;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Артем\Documents\GitHub\roominator\Roominator\_Imports.razor"
+#line 10 "C:\Users\balan\Documents\GitHub\roominator\Roominator\_Imports.razor"
 using Roominator.Shared;
 
 #line default
