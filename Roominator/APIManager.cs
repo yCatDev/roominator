a96f14853ce7d2aacf83e6ad22f87b7117ff3369
Wind6365 <30289245+Wind6365@users.noreply.github.com>
2021-05-12 21:34:05 +0300

    API continue check

--- Roominator/APIManager.cs
+++ Roominator/APIManager.cs
@@ -2,23 +2,52 @@ using Microsoft.AspNetCore.Components;
 using Google.Apis;
 using Microsoft.AspNetCore.Http;
 using System.Web;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using System.Collections.Generic;
+using System;
 
 namespace Roominator {
     class APIManager
     {
-        private const string scope = "https://www.googleapis.com/auth/userinfo.email";
-        private const string redirectURI = "https://roominator-nure.herokuapp.com/menu";
-        private const string redURI = "https%3A%2F%2Froominator-nure.herokuapp.com%2Fmenu";
-        private const string clientId = "636057004195-qufc1kso44du6lpbp191vdmdvogco237.apps.googleusercontent.com";
-        public string createRequest() {
+        private const string SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+        private const string REDIRECT_URI = "https://roominator-nure.herokuapp.com/menu";
+        private const string CLIENT_ID = "636057004195-qufc1kso44du6lpbp191vdmdvogco237.apps.googleusercontent.com";
+        private const string CLIENT_SECRET = "QNn_2SkcZkqgmCItvB-0Lb7N";
+        [Inject]
+        public NavigationManager NavManager { get; set; }
+        public string createRequest(string uri) {
             string request = $"https://accounts.google.com/o/oauth2/v2/auth?" +
-                $"redirect_uri={redirectURI}&" +
-                $"client_id={clientId}&" +
-                $"scope={scope}&" +
+                $"redirect_uri={uri}menu&" +
+                $"client_id={CLIENT_ID}&" +
+                $"scope={SCOPE}&" +
                 $"response_type=code";
             return request;
         }
 
+        public string getCode(string uri) {
+            string code = "";
+            Regex regex = new Regex("(code.+?)&");
+            MatchCollection matches = regex.Matches(uri);
+            foreach (Match match in matches) {
+                code = match.Value;
+            }
+            return code;
+        }
+
+        public async System.Threading.Tasks.Task<HttpResponseMessage> sendRequestToExchangeAccessTokenAsync(string code) {
+            HttpClient client = new HttpClient();
+            var values = new Dictionary<string, string>
+                {
+                    { "client_id", $"{CLIENT_ID}&" },
+                    { "client_secret", $"{CLIENT_SECRET}&" },
+                    { "grant_type", $"authorization_code&" },
+                    { "redirect_uri", $"{REDIRECT_URI}&" }
+                };
+            var answer = await client.PostAsync("https://oauth2.googleapis.com/token", new FormUrlEncodedContent(values));
+            Console.WriteLine(answer);
+            return answer;
+        }
     }
 }
 
