707e93dc63b8665e47680b9334d32dda606a267f
Wind6365 <30289245+Wind6365@users.noreply.github.com>
2021-05-19 23:04:51 +0300

    Amen(Changed Auth and Reg)

---
+++ Roominator/Areas/Identity/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace Roominator {
+    public class AppDbContext : IdentityDbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+        
+        }
+    }
+}
--- Roominator/Startup.cs
+++ Roominator/Startup.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Providers.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,8 +34,12 @@ namespace Roominator
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+            services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            
             services.AddSingleton<WeatherForecastService>();
             services.AddServerSideBlazor().AddCircuitOptions(options => {
                 if (_env.IsDevelopment())
@@ -61,12 +70,17 @@ namespace Roominator
             provider.Mappings.Remove(".symbols.json");
             provider.Mappings[".symbols.json"] = "application/octet-stream";
             app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = provider });
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
