eb31e4291469dcee149ac598b1edf2c3e8fdc234
DrearySasha1222 <oleksandr.dudnyk@nure.ua>
2021-05-25 23:01:08 +0300

    Register fix

---
+++ Roominator/wwwroot/javascript/Register.js
@@ -0,0 +1,91 @@
+﻿window.onload = funonload;
+
+function funonload() {
+    var summary = document.getElementById("RegisterSummary").childNodes[0].childNodes[0].innerHTML;
+    if (summary.length != "") {
+        SetErrorLine(summary, true)
+    }
+
+    var target = document.getElementById("RegAcc");
+
+    const config = {
+        childList: true,
+        subtree: true
+    };
+
+    const callback = function (mutationsList, observer) {
+        var errors = [];
+        //Ошибка в мыле
+        if (document.getElementById("SignupMail-error")) {
+            SetWarningIcon("SignupMailWarning", true)
+            errors.push(document.getElementById("SignupMail-error").innerHTML);
+        }
+        else {
+            SetWarningIcon("SignupMailWarning", false)
+        }
+        //Ошибка в пароле
+        if (document.getElementById("SignupPassword-error")) {
+            SetWarningIcon("SignupPassworfWarning", true)
+            errors.push(document.getElementById("SignupPassword-error").innerHTML);
+        }
+        else {
+            SetWarningIcon("SignupPassworfWarning", false)
+        }
+        //Ошибка в пароле 2
+        if (document.getElementById("SignupConfirmPassword-error")) {
+            SetWarningIcon("SignupConfirmPasswordWarning", true)
+            errors.push(document.getElementById("SignupConfirmPassword-error").innerHTML);
+        }
+        else {
+            SetWarningIcon("SignupConfirmPasswordWarning", false)
+        }
+        var mql = window.matchMedia("(orientation: landscape)")
+        if (errors.length > 0) {
+            SetErrorLine(errors.join( (mql) ? (" | ") : ("<br>")), true)
+        }
+        else {
+            SetErrorLine(" ", false);
+        }
+
+    };
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(target, config);
+
+
+}
+
+
+function SetWarningIcon(id, enable) {
+    elem = document.getElementById(id);
+    if (enable == true) {
+        if (elem.classList.contains("Hidden") || elem.classList.contains("Hide")) {
+            elem.className = "AuthWarningIcon Show"
+        }
+    }
+    else {
+        if (elem.classList.contains("Show")) {
+            elem.className = "AuthWarningIcon Hide"
+        }
+    }
+}
+
+function SetErrorLine(text, enable) {
+    elem = document.getElementById("RegisterErrLine");
+
+    if (enable == true) {
+        elem.innerHTML = text;
+        if (elem.classList.contains("AuthCollapsedLine")
+            || elem.classList.contains("AuthCollapseLine")) {
+            elem.className = "AuthErrorMessage Unselectable AuthExpandLine"
+        }
+    }
+    else {
+        
+        if (elem.classList.contains("AuthExpandLine")) {
+            elem.className = "AuthErrorMessage Unselectable AuthCollapseLine"
+        }
+        elem.innerHTML = " ";
+    }
+}
