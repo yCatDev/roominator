ad759ee152f57b53d738053e24ed9a99a5842ab0
DrearySasha1222 <oleksandr.dudnyk@nure.ua>
2021-05-25 18:49:20 +0300

    Login Fixed

---
+++ Roominator/wwwroot/javascript/Login.js
@@ -0,0 +1,83 @@
+﻿window.onload = funonload;
+
+function funonload() {
+    var summary = document.getElementById("LoginSummary").childNodes[0].childNodes[0].innerHTML;
+    if (summary.length != "") {
+        SetErrorLine(summary, true)
+    }
+
+    var target = document.getElementById("account");
+
+    const config = {
+        childList: true,
+        subtree: true
+    };
+
+    const callback = function (mutationsList, observer) {
+        var errors = [];
+        //Ошибка в мыле
+        if (document.getElementById("SigninMail-error")) {
+            SetWarningIcon("SigninMailWarning", true)
+            errors.push(document.getElementById("SigninMail-error").innerHTML);
+        }
+        else {
+            SetWarningIcon("SigninMailWarning", false)
+        }
+        //Ошибка в пароле
+        if (document.getElementById("SigninPassword-error")) {
+            SetWarningIcon("SigninPassworfWarning", true)
+            errors.push(document.getElementById("SigninPassword-error").innerHTML);
+        }
+        else {
+            SetWarningIcon("SigninPassworfWarning", false)
+        }
+
+        if (errors.length > 0) {
+            SetErrorLine(errors.join(" | "), true)
+        }
+        else {
+            SetErrorLine(" ", false);
+        }
+
+    };
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(target, config);
+
+
+}
+
+
+function SetWarningIcon(id, enable) {
+    elem = document.getElementById(id);
+    if (enable == true) {
+        if (elem.classList.contains("Hidden") || elem.classList.contains("Hide")) {
+            elem.className = "AuthWarningIcon Show"
+        }
+    }
+    else {
+        if (elem.classList.contains("Show")) {
+            elem.className = "AuthWarningIcon Hide"
+        }
+    }
+}
+
+function SetErrorLine(text, enable) {
+    elem = document.getElementById("LoginErrLine");
+
+    if (enable == true) {
+        elem.innerHTML = text;
+        if (elem.classList.contains("AuthCollapsedLine")
+            || elem.classList.contains("AuthCollapseLine")) {
+            elem.className = "AuthErrorMessage Unselectable AuthExpandLine"
+        }
+    }
+    else {
+        
+        if (elem.classList.contains("AuthExpandLine")) {
+            elem.className = "AuthErrorMessage Unselectable AuthCollapseLine"
+        }
+        elem.innerHTML = " ";
+    }
+}
